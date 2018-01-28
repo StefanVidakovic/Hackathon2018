bedd612987e3d701569801f19d9efd51346c1100
StefanVidakovic <vidakovi@ualberta.ca>
2018-01-27 19:18:14 -0700

    Update PhysicsEngine.java

--- PhysicsEngine.java
+++ PhysicsEngine.java
@@ -6,7 +6,7 @@ public class PhysicsEngine {
 	Deque<GameObject> verificationStack = new ArrayDeque<GameObject>();
 	Deque<GameObject> commitStack = new ArrayDeque<GameObject>();
 
-<<<<<<< HEAD
+
 	public Point calculateCords(GameObject g) {
 		int newX = g.bottomLeftCorner.x + (int) g.xAcc*4;
 		int newY = g.bottomLeftCorner.y + (int) g.yAcc*4;
@@ -42,7 +42,7 @@ public class PhysicsEngine {
 			 */
 			 
 		}
-=======
+
 	public boolean intersecting(GameObject obstacle, GameObject player) {
 		if(obstacle.bottomLeftCorner.compareTo(player.newbottomLeftCorner)>0 //this means obj2 is "greater"
 				&&(obstacle.topLeftCorner.compareTo(player.newbottomLeftCorner)<0
@@ -96,67 +96,5 @@ public class PhysicsEngine {
 		//Return true, if need updateing.
 		//False if unchanged.
 		return false;
->>>>>>> origin/master
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	public PhysicsEngine() {
-//		Deque<GameObject> toDo = new ArrayDeque<GameObject>();
-//		Deque<GameObject> toUpdate = new ArrayDeque<GameObject>();
-//	}
-//	
-//	public boolean intersecting(GameObject obj1, GameObject obj2) {
-//		if(obj1.bottomLeftCorner.compareTo(obj2.bottomLeftCorner)>0
-//				&&(obj1.topLeftCorner.compareTo(obj2.bottomLeftCorner)<0
-//						&&obj1.bottomRightCorner.compareTo(obj2.bottomLeftCorner)<0)) {
-//			return true;
-//		}
-//		if(obj1.bottomLeftCorner.compareTo(obj2.bottomRightCorner)>0
-//				&&(obj1.topLeftCorner.compareTo(obj2.bottomRightCorner)<0
-//						&&obj1.bottomRightCorner.compareTo(obj2.bottomRightCorner)<0)) {
-//			return true;
-//		}
-//		if(obj1.bottomLeftCorner.compareTo(obj2.topLeftCorner)>0
-//				&&(obj1.topLeftCorner.compareTo(obj2.topLeftCorner)<0
-//						&&obj1.bottomRightCorner.compareTo(obj2.topLeftCorner)<0)) {
-//			return true;
-//		}
-//		if(obj1.bottomLeftCorner.compareTo(obj2.topRightCorner)>0
-//				&&(obj1.topLeftCorner.compareTo(obj2.topRightCorner)<0
-//						&&obj1.bottomRightCorner.compareTo(obj2.topRightCorner)<0)) {
-//			return true;
-//		}
-//		
-//		return false;
-//	}
-//	public boolean calculateNewLocation(GameObject a) {
-//		//Return true, if need updateing.
-//		//False if unchanged.
-//		return false;
-//	}
-//	
-//	public void updateAll() {
-//		
-//	}
-}
